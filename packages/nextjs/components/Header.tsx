1884768cf4ca7b9b2afe5e481f4afdd59bdc4bfe
Kenny Rch <ken@99nth.com>
2026-09-10 10:58:41 +0300

    docs: Update README and site metadata

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -98,8 +98,8 @@ export const Header = () => {
             />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-ETH</span>
-            <span className="text-xs">Ethereum dev stack</span>
+            <span className="font-bold leading-tight">IPT</span>
+            <span className="text-xs">The interplanetary token</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap h-full m-0 p-0 list-none">
